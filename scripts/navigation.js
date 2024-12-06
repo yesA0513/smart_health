771b63a5dc6ba174f4b6386f6599954668003aad
yesA0513 <kimnoa0513@gmail.com>
2024-12-07 05:13:49 +0900

    Update: 홈으로 버튼 수정:

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -2,7 +2,7 @@
 function goToHome() {
     // localStorage에서 지정된 경로 가져오기
     const storagePath = document.querySelector('meta[name="localStorage-path"]')?.content || '/';
-    const homePath = storagePath + 'main.html';
+    const homePath = storagePath + 'index.html';
     
     // 페이지 이동 전에 부드러운 전환 효과
     document.querySelector('.content-wrapper').style.opacity = '0';
